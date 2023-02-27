9147a88bc63f788dd4057214d534cab3786359d6
Qobiljon <123439008+IskandarovQobiljon@users.noreply.github.com>
2023-02-27 12:22:39 +0500

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+// Special Countdown 
+let daysItem = document.querySelector("#days");
+let hoursItem = document.querySelector("#hours");
+let minItem = document.querySelector("#min");
+let secItem = document.querySelector("#sec");
+
+let countDown = () => {
+    let futureDate = new Date("30 May 2023");
+    let currentDate = new Date();
+    let myDate = futureDate - currentDate;
+
+    let days = Math.floor(myDate / 1000 / 60 / 60 / 24);
+    let hours = Math.floor(myDate / 1000 / 60 / 60) % 24;
+    let min = Math.floor(myDate / 1000 / 60) % 60;
+    let sec = Math.floor(myDate / 1000) % 60;
+
+    daysItem.innerHTML = days;
+    hoursItem.innerHTML = hours;
+    minItem.innerHTML = min;
+    secItem.innerHTML = sec;
+}
+
+countDown()
+
+setInterval(countDown, 1000)
+
+// nav hide  
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function (a) {
+    a.addEventListener("click", function () {
+        navCollapse.classList.remove("show");
+    })
+});
+
+// // Scroll Back To Top 
+$('a[href*="#"]').on('click', function (e) {
+
+    e.preventDefault();
+
+    $('html, body').animate({
+
+        scrollTop: $($(this).attr('href')).offset().top,
+
+    },
+        500,
+        'linear'
+    );
+
+});
+
+
